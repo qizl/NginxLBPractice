a908a79f1e61b73377899a5dd5ea72768a878779
qizl <q@qizl.cn>
2018-09-29 12:13:00 +0800

    -

---
+++ src/API3/Controllers/ValuesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            var assembly = System.Reflection.Assembly.GetAssembly(typeof(ValuesController));
+
+            return assembly.GetName().Name;
+        }
+    }
+}
